7cec10dac8b898b8e0840402d54efbce9c46edac
Czryourbro1 <cezarandrei2004@gmail.com>
2022-05-27 16:54:01 +0300

    initial commit

---
+++ diagnosis/proposed_symptoms.js
@@ -0,0 +1,15 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "priaid-symptom-checker-v1.p.rapidapi.com",
+    "X-RapidAPI-Key": "08805db294msha9cd99bf0490bc9p1509f3jsnc510eeb10d9e",
+  },
+};
+
+fetch(
+  "https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms/proposed?gender=male&year_of_birth=1984&language=en-gb&symptoms=%5B234%2C11%5D",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
